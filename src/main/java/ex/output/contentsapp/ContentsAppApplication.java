42f6997b1a71501d8e631f3b2f489941552714db
hirm159 <hiro159m@gmail.com>
2022-03-12 09:03:25 +0900

    first commit

---
+++ src/main/java/ex/output/contentsapp/ContentsAppApplication.java
@@ -0,0 +1,13 @@
+package ex.output.contentsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ex/output/contentsapp/ContentsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.output.contentsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
